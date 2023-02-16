7a95884a5b0c702ab1a825ddc8975b46b13ff8a6
nommadj <nommadj@gmail.com>
2023-02-16 11:10:35 -0800

    4 puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#!/bin/bash
-include <stdio.h>
+#include <stdio.h>
 /**
- *  *main - main block
- *   *return:0
- *    */
+ * *main - Entry point
+ * *Return: Always 0 (Success)
+ * */
+
 int main(void)
 {
-		puts("\""Programming is like building a multilingual puzzle");
-			return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+					return (0);
 }
