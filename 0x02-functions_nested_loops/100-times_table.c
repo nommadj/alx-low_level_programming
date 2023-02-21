5855d56658edd56e3ba5efe2af23bdaa9cbad737
nommadj <nommadj@gmail.com>
2023-02-21 09:10:45 -0800

    many errors

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,9 +1,13 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
- */
+ * print_times_table - Prints the n times table, starting with 0
+ *
+ * @n: Number of the times table
+ *
+ * Description: This function prints the n times table,
+ * starting with 0.
+ **/
 void print_times_table(int n)
 {
     int i, j, k;
@@ -16,9 +20,7 @@ void print_times_table(int n)
             {
                 k = j * i;
                 if (j == 0)
-                {
                     _putchar(k + '0');
-                }
                 else if (k < 10 && j != 0)
                 {
                     _putchar(',');
