a5e5afb800ea5dc6ba2bd37d239bf65f5f8d9fc7
nommadj <nommadj@gmail.com>
2023-02-23 04:28:34 -0800

    8 prnt

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,4 +1,10 @@
-print_square(int size)
+#include "main.h"
+
+/**
+ * print_square - prints a square, followed by a new line;
+ * @size: size of the square
+ */
+void print_square(int size)
 {
  if (size <= 0)
  {
