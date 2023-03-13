8d86b03d953437cea1752e8b54410e43cbb207f8
nommadj <nommadj@gmail.com>
2023-03-13 01:20:05 -0700

    crate array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,55 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *create_array - creates an array of chars, and initializes
+ *  it with a specific char.
+ *  @c: char to initialize
+ *  @size: number of bytes to allocate
+ *
+ *  Return: a pointer to the array or null if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array = malloc(size);
+
+	if (size == 0 || array == 0)
+		return (NULL);
+	while (size--)
+		array[size] = c;
+	return (array);
+}
+
+vi 1-strdup.c
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * *_strdup - copies the string given as parameter
+ * @str: string to duplicate
+ *
+ * Return: pointer to the copied string (Success), NULL (Error)
+ */
+char *_strdup(char *str)
+{
+	char *duplicate;
+	unsigned int i, len;
+
+	i = 0;
+	len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+	duplicate = malloc(sizeof(char) * (len + 1));
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	while ((duplicate[i] = str[i]) != '\0')
+		i++;
+
+	return (duplicate);
+}
