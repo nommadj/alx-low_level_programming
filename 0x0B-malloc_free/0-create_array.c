4577d297f83c11cd6e831a83d98330457e900fe1
nommadj <nommadj@gmail.com>
2023-03-13 01:31:32 -0700

    create aray

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,37 +19,3 @@ char *create_array(unsigned int size, char c)
 		array[size] = c;
 	return (array);
 }
-
-vi 1-strdup.c
-#include <stdlib.h>
-#include "holberton.h"
-
-/**
- * *_strdup - copies the string given as parameter
- * @str: string to duplicate
- *
- * Return: pointer to the copied string (Success), NULL (Error)
- */
-char *_strdup(char *str)
-{
-	char *duplicate;
-	unsigned int i, len;
-
-	i = 0;
-	len = 0;
-
-	if (str == NULL)
-		return (NULL);
-
-	while (str[len])
-		len++;
-	duplicate = malloc(sizeof(char) * (len + 1));
-
-	if (duplicate == NULL)
-		return (NULL);
-
-	while ((duplicate[i] = str[i]) != '\0')
-		i++;
-
-	return (duplicate);
-}
