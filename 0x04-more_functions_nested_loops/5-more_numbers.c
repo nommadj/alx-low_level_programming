0457413f87f912c6bf2da011452bad205f4a5468
nommadj <nommadj.com>
2023-10-12 03:09:16 -0700

    4 more loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,21 @@
 #include "main.h"
-
 /**
- * more_numbers - entry point
- * Return:void
+ * more_numbers - A function that
+ * prints the numbers 0 to 14
  */
-
-void more_numbers(void);
+void more_numbers(void)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-		int j;
-
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			if (j >= 10)
+			{
+				_putchar('1');
+			}
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
