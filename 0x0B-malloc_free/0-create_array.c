9c7d68a3081c8190ec562e3c017b94af08917c54
nommadj <nommadj.com>
2023-10-29 23:26:15 -0700

    free malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,37 +1,28 @@
 #include "main.h"
-
+#include "stdlib.h"
 /**
- * create_array - creates an array of chars, and
- * initializes it with a specific char.
- *@size: Size of the array
- *@c: Character to insert
- *Return: NULL if size is zero or if it fails,
- *pointer to array if everything is normal.
-
- *FUNCTIONALITY *
-
- * 1. First, we check if the size is 0. If it is, we return NULL.
- * 2. Then, we allocate memory for the array.
- * 3. If the allocation fails, we return NULL.
- * 4. If the allocation succeeds, we loop through the array and set each element to the value of c.
- * 5. Finally, we return the array.
- * Time Complexity: O(n)
-
-*/
+  * create_array - a function to create array of chars, and initialize,
+  * it with a specific char.
+  * @size: size of the array to create
+  * @c: char to initialize the array c
+  *
+  * Return: pointer to the array (Success), NULL (Error)
+  */
 char *create_array(unsigned int size, char c)
 {
-	char *array;
-	unsigned int index;
+	char *s;
+	unsigned int i = 0;
 
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
 	if (size == 0)
 		return (NULL);
-	array = malloc(sizeof(char) * size);
-
-	if (array == NULL)
-		return (NULL);
-
-	for (index = 0; index < size; index++)
-		array[index] = c;
-
-	return (array);
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	return (s);
 }
--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,47 +1,32 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
- * _strdup - returns a pointer to a newly allocated
- *space in memory, which contains a copy of the
- *string given as a parameter.
- *@str:String to be copied
- *
- * Return: NULL in case of error, pointer to allocated
- * space
- *
- * FUNCTIONALITY
- *
- * 1. First, we check if the string is NULL.  If it is, we return NULL.
- * 2.  Next, we loop through the string to find the length of the string.
- * 3.  We allocate memory for the copy of the string.
- * 4.  We check if the memory was allocated.  If it wasn’t, we return NULL.
- * 5.  We loop through the string again, this time copying the string into the new memory.
- * 6.  We add a null terminator to the end of the string.
- * 7.  We return the copy of the string.
- *
- * Time Complexity: O(n)
- */
+  *_strdup - copies the string given as parameter
+  * @str: string to duplicate
+  *
+  * Return: pointer to the array (Success), NULL (Error)
+  */
 char *_strdup(char *str)
 {
-	char *cpy;
-	int index, len;
+	char *cp;
+	unsigned int i, len;
+
+	i = 0;
+	len = 0;
 
 	if (str == NULL)
 		return (NULL);
 
-	for (index = 0; str[index]; index++)
+	while (str[len])
 		len++;
-	cpy = malloc(sizeof(char) * (len + 1));
 
-	if (cpy == NULL)
+	cp = malloc(sizeof(char) * (len + 1));
+
+	if (cp == NULL)
 		return (NULL);
 
-	for (index = 0; str[index]; index++)
-	{
-		cpy[index] = str[index];
-	}
+	while ((cp[i] = str[i]) != '\0')
+		i++;
 
-	cpy[len] = '\0';
-
-	return (cpy);
+	return (cp);
 }
